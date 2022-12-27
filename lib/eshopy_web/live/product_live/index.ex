5953584125392010705596d5d3ec974dc4b393c2
Mateusz <babskimateusz@gmail.com>
2022-12-27 23:32:13 +0100

    cleaning mount/3

--- lib/eshopy_web/live/product_live/index.ex
+++ lib/eshopy_web/live/product_live/index.ex
@@ -6,13 +6,13 @@ defmodule EshopyWeb.ProductLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    brand_list = Enum.map(Catalog.list_brands(), fn %{id: id, name: name} ->
-        [value: id, key: name]
-    end)
+    # brand_list = Enum.map(Catalog.list_brands(), fn %{id: id, name: name} ->
+    #     [value: id, key: name]
+    # end)
 
     {:ok, socket
           |> assign(:products, list_products())
-          |> assign(:brands, brand_list)
+          |> assign(:brands, Catalog.list_brands())
     }
   end
 
