826c548229d005e4ccb9e00864e68bd01fda83cd
Mateusz <babskimateusz@gmail.com>
2022-12-29 22:24:54 +0100

    image_upload corrected

--- lib/eshopy_web/live/product_live/form_component.ex
+++ lib/eshopy_web/live/product_live/form_component.ex
@@ -13,63 +13,68 @@ defmodule EshopyWeb.ProductLive.FormComponent do
      |> assign(:changeset, changeset)
      |> assign(:brands, Catalog.list_brands())
      |> assign(:categories, Catalog.list_categories())
-     |> assign(:image_upload, nil)
+     |> assign(:image_upload, nil )
      |> allow_upload(:image,
       accept: ~w(.jpg .jpeg .png),
       max_entries: 1,
+      max_file_size: 9_000_000,
       auto_upload: true,
       progress: &handle_progress/3)}
   end
 
-  def handle_progress(:image, entry, socket) do
-    if entry.done? do
-      path = consume_uploaded_entry(socket, entry, &upload_static_file(&1, socket))
-      IO.inspect(path)
-      {:noreply,
-      socket
-      |> put_flash(:info, "File #{entry.client_name} uploaded.")
-      |> assign(:image_upload, path)}
-    else
-      {:noreply, socket}
-    end
-  end
-
-  defp upload_static_file(%{path: path}, socket) do
-    root_path = "priv/static/images/uploads"
-    dest = Path.join(root_path, Path.basename(path))
-    File.cp!(path, dest)
-    Routes.static_path(socket, "images/uploads/#{Path.basename(dest)}")
-  end
-
   @impl true
   def handle_event("validate", %{"product" => params}, socket) do
-    changeset =
-      socket.assigns.product
-      |> Catalog.change_product(params)
-      |> Map.put(:action, :validate)
+   changeset =
+     socket.assigns.product
+     |> Catalog.change_product(params)
+     |> Map.put(:action, :validate)
 
-    {:noreply, assign(socket, :changeset, changeset)}
+   {:noreply, assign(socket, :changeset, changeset)}
   end
 
   def handle_event("save", %{"product" => params}, socket) do
     save_product(socket, socket.assigns.action, params)
   end
 
+  defp handle_progress(:image, entry, socket) do
+    if entry.done? do
+      path =
+        consume_uploaded_entry(
+          socket,
+          entry,
+          &upload_static_file(&1, socket)
+        )
+      {:noreply,
+        socket
+        |> put_flash(:info, "file #{entry.client_name} uploaded")
+        |> assign(:image_upload, path)}
+    else
+      {:noreply,
+        socket
+        |> put_flash(:info, "file upload error")}
+    end
+  end
+
+  defp upload_static_file(%{path: path}, socket) do
+    dest = Path.join("priv/static/images/", Path.basename(path))
+    File.cp!(path, dest)
+    {:ok, Routes.static_path(socket, "/images/#{Path.basename(dest)}")}
+  end
+
   defp save_product(socket, :edit, params) do
     case Catalog.update_product(socket.assigns.product, product_params(socket, params)) do
-      {:ok, _product} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Product updated successfully")
-         |> push_redirect(to: socket.assigns.return_to)}
+    {:ok, _product} ->
+      {:noreply,
+      socket
+        |> put_flash(:info, "Product updated successfully")
+        |> push_redirect(to: socket.assigns.return_to)}
 
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, :changeset, changeset)}
+    {:error, %Ecto.Changeset{} = changeset} ->
+      {:noreply, assign(socket, :changeset, changeset)}
     end
   end
 
   defp save_product(socket, :new, params) do
-    IO.inspect(product_params(socket, params))
     case Catalog.create_product(product_params(socket, params)) do
       {:ok, _product} ->
         {:noreply,
