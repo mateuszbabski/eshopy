61906b9a931f60f79c3c8828a23b7cbd7092038c
Mateusz <babskimateusz@gmail.com>
2023-01-17 08:33:10 +0100

    working on cart view

--- lib/eshopy_web/live/cart_live/show.ex
+++ lib/eshopy_web/live/cart_live/show.ex
@@ -2,16 +2,25 @@ defmodule EshopyWeb.CartLive.Show do
   use EshopyWeb, :live_view
 
   alias Eshopy.Accounts
+  alias Eshopy.Catalog
   alias Eshopy.ShoppingCart
 
   @impl true
   def mount(_params, %{"user_token" => user_token}, socket) do
     user = Accounts.get_user_by_session_token(user_token)
+    cart = ShoppingCart.get_cart_by_user_id(user.id)
 
     {:ok,
     socket
     |> assign(:current_user, user)
-    |> assign(:carts, ShoppingCart.get_cart_by_user_id(user.id))}
+    |> assign(:cart, ShoppingCart.get_cart_by_user_id(user.id))
+    |> assign(:cart_items, ShoppingCart.list_cart_items(cart.id))
+    |> assign(:product, Catalog.list_products())
+    }
+  end
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
   end
 
   @impl true
