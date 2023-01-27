4cb48915db62f310693d510207bb84c367bc2088
Mateusz <babskimateusz@gmail.com>
2023-01-27 23:53:59 +0100

    customer data form component

--- lib/eshopy_web/live/customer_live/form_component.ex
+++ lib/eshopy_web/live/customer_live/form_component.ex
@@ -1,16 +1,26 @@
 defmodule EshopyWeb.CustomerLive.FormComponent do
-  use EshopyWeb, :live_component
+  use EshopyWeb, :live_view
 
   alias Eshopy.Accounts
+  alias Eshopy.Accounts.Customer
 
   @impl true
-  def update(%{customer: customer} = assigns, socket) do
-    changeset = Accounts.change_customer(customer)
+  def mount(_params, %{"user_token" => user_token}, socket) do
+    changeset = Accounts.change_customer(%Customer{})
+    user = Accounts.get_user_by_session_token(user_token)
 
     {:ok,
-     socket
-     |> assign(assigns)
-     |> assign(:changeset, changeset)}
+    socket
+    |> assign(:current_user, user)
+    |> assign(:customer, %Customer{})
+    |> assign(:changeset, changeset)}
+  end
+
+  def mount(_params, _session, socket) do
+    {:ok,
+        socket
+        |> put_flash(:info, "You must be logged in")
+        |> redirect(to: Routes.home_path(socket, :home))}
   end
 
   @impl true
@@ -24,29 +34,35 @@ defmodule EshopyWeb.CustomerLive.FormComponent do
   end
 
   def handle_event("save", %{"customer" => customer_params}, socket) do
-    save_customer(socket, socket.assigns.action, customer_params)
-  end
+    case Accounts.get_customer_data_by_user_id(socket.assigns[:current_user].id) do
+      nil ->
+        save_customer(socket, customer_params)
 
-  defp save_customer(socket, :edit, customer_params) do
-    case Accounts.update_customer(socket.assigns.customer, customer_params) do
-      {:ok, _customer} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Customer updated successfully")
-         |> push_redirect(to: socket.assigns.return_to)}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, :changeset, changeset)}
+      %Customer{} = customer ->
+        update_customer(socket, customer, customer_params)
     end
   end
 
-  defp save_customer(socket, :new, customer_params) do
-    case Accounts.create_customer(customer_params) do
+  defp update_customer(socket, customer, customer_params) do
+    case Accounts.update_customer(socket.assigns[:current_user], customer, customer_params) do
+      {:ok, _customer} ->
+        {:noreply,
+        socket
+        |> put_flash(:info, "Customer updated successfully")
+        |> push_redirect(to: Routes.complete_order_show_customer_data_path(socket, :show_customer_data))}
+
+        {:error, %Ecto.Changeset{} = changeset} ->
+          {:noreply, assign(socket, :changeset, changeset)}
+    end
+  end
+
+  defp save_customer(socket, customer_params) do
+    case Accounts.create_customer(socket.assigns[:current_user], customer_params) do
       {:ok, _customer} ->
         {:noreply,
          socket
          |> put_flash(:info, "Customer created successfully")
-         |> push_redirect(to: socket.assigns.return_to)}
+         |> push_redirect(to: Routes.complete_order_show_customer_data_path(socket, :show_customer_data))}
 
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
