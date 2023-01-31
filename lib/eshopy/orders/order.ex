f7a2a2c71745250abf9eaecbc70dd5eb02b2d377
Mateusz <babskimateusz@gmail.com>
2023-01-31 23:21:35 +0100

    added order status

--- lib/eshopy/orders/order.ex
+++ lib/eshopy/orders/order.ex
@@ -1,9 +1,16 @@
 defmodule Eshopy.Orders.Order do
   use Ecto.Schema
   import Ecto.Changeset
+  import EctoEnum
+
+  defenum(StatusEnum, :status, [
+    :in_progress,
+    :completed
+  ])
 
   schema "orders" do
     field :total_price, :decimal
+    field :status, StatusEnum, default: :in_progress
 
     belongs_to :shipping, Eshopy.Delivery.Shipping, foreign_key: :shipping_id
     belongs_to :user, Eshopy.Accounts.User, foreign_key: :user_id
@@ -19,5 +26,15 @@ defmodule Eshopy.Orders.Order do
     |> cast(attrs, [:total_price])
     |> validate_required([:total_price])
     |> validate_number(:total_price, greater_than: 0)
+  end
+
+  def status_changeset(order, attrs) do
+    order
+    |> cast(attrs, [:status])
+    |> validate_required([:status])
+    |> case do
+      %{changes: %{status: _}} = changeset -> changeset
+      %{} = changeset -> add_error(changeset, :status, "Error with changing status")
+    end
   end
 end
--- priv/repo/migrations/20230122102520_create_orders.exs
+++ priv/repo/migrations/20230122102520_create_orders.exs
@@ -1,9 +1,14 @@
 defmodule Eshopy.Repo.Migrations.CreateOrders do
   use Ecto.Migration
 
+  alias Eshopy.Orders.Order.StatusEnum
+
   def change do
+    StatusEnum.create_type()
+
     create table(:orders) do
       add :total_price, :decimal
+      add :status, StatusEnum.type(), null: false
 
       add :shipping_id, references(:shippings, on_delete: :nothing)
       add :user_id, references(:users, on_delete: :delete_all)
