327de7e65e0f81883809e1e1a73db5fac72e91f4
Mateusz <babskimateusz@gmail.com>
2023-02-03 10:47:05 +0100

    simple ui admin dashboard

--- lib/eshopy_web/components/admin_dashboard/admin_dashboard_live.ex
+++ lib/eshopy_web/components/admin_dashboard/admin_dashboard_live.ex
@@ -1,10 +1,33 @@
 defmodule EshopyWeb.AdminDashboardLive do
   use EshopyWeb, :live_view
 
-  # @impl true
-  # def mount(_params, _session, socket) do
-  #   {:ok, assign(socket, :products, list_products())}
-  # end
+  alias Eshopy.Accounts
+
+  @impl true
+  def mount(_params, %{"user_token" => user_token}, socket) do
+    user = Accounts.get_user_by_session_token(user_token)
+
+    case user.role do
+      :user ->
+        {:ok,
+          socket
+          |> assign(:current_user, user)
+          |> put_flash(:info, "Unauthorized")
+          |> redirect(to: Routes.home_path(socket, :home))}
+
+      :admin ->
+        {:ok,
+          socket
+          |> assign(:current_user, user)}
+    end
+  end
+
+  def mount(_params, _session, socket) do
+    {:ok,
+      socket
+      |> put_flash(:info, "Unauthorized")
+      |> redirect(to: Routes.home_path(socket, :home))}
+  end
 
   # @impl true
   # def handle_event() do
