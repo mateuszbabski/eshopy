708abdf6d5c0bf4bd70979b0264eb0a978a43e52
Mateusz <babskimateusz@gmail.com>
2023-02-15 21:58:41 +0100

    subscribe user presence

--- lib/eshopy_web/live/admin_live/users/users.ex
+++ lib/eshopy_web/live/admin_live/users/users.ex
@@ -2,6 +2,9 @@ defmodule EshopyWeb.AdminLive.Users do
   use EshopyWeb, :live_view
 
   alias Eshopy.Accounts
+  alias EshopyWeb.UserPresenceLive
+
+  @topic "user_presence"
 
   @impl true
   def mount(_params, %{"user_token" => user_token}, socket) do
@@ -16,9 +19,12 @@ defmodule EshopyWeb.AdminLive.Users do
           |> redirect(to: Routes.home_index_path(socket, :index))}
 
       :admin ->
+        EshopyWeb.Endpoint.subscribe(@topic)
+
         {:ok,
           socket
           |> assign(:current_user, user)
+          |> assign(:user_presence_component_id, "user_presence")
           |> assign(:users, Accounts.list_users())}
     end
   end
@@ -28,6 +34,23 @@ defmodule EshopyWeb.AdminLive.Users do
       socket
       |> put_flash(:info, "Unauthorized")
       |> redirect(to: Routes.home_index_path(socket, :index))}
+  end
+
+  @impl true
+  # def handle_info(%{event: "presence_state"}, socket) do
+  #   send_update(
+  #     UserPresenceLive,
+  #     id: socket.assigns.user_presence_component_id)
+
+  #   {:noreply, socket}
+  # end
+
+  def handle_info(%{event: "presence_diff"}, socket) do
+    send_update(
+      UserPresenceLive,
+      id: socket.assigns.user_presence_component_id)
+
+    {:noreply, socket}
   end
 
   @impl true
